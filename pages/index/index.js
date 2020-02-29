1a299067ea965e6d0ebe24e1e5e4f8524e0647d6
vueyyy_glb <858078219@qq.com>
2020-02-29 19:53:45 +0800

    updateHomeDoctor

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,18 @@ const app = getApp()
 
 Page({
   data: {
+    datas:[
+      { name: '皮肤科', urlImage:'/images/home_doctor/physical_signs.png'},
+      { name: '普内科', urlImage: '/images/home_doctor/physical_signs.png' },
+      { name: '普外科', urlImage: '/images/home_doctor/physical_signs.png' },
+      { name: '中医科', urlImage: '/images/home_doctor/physical_signs.png' },
+      { name: '泌尿科', urlImage: '/images/home_doctor/physical_signs.png' },
+      { name: '消化内科', urlImage: '/images/home_doctor/physical_signs.png' },
+      { name: '骨科', urlImage: '/images/home_doctor/physical_signs.png' },
+      { name: '更多', urlImage: '/images/home_doctor/physical_signs.png' },
+
+
+    ]
   },
   onLoad: function () {
   }, 
